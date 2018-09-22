fb8398607c0bac335461a9608a83937db7503107
stlbucket <stlbucket@gmail.com>
2018-09-22 11:07:35 -0700

    init commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <graphiql></graphiql>
+  </div>
+</template>
+
+<script>
+import Graphiql from 'graphiql'
+
+export default {
+  name: 'app',
+  components: {
+    Graphiql
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue.config.js
@@ -0,0 +1,42 @@
+const FlowBabelWebpackPlugin = require('flow-babel-webpack-plugin');
+const path = require('path')
+
+module.exports = {
+  pluginOptions: {
+    apollo: {
+      enableMocks: true,
+      enableEngine: false
+    }
+  },
+  devServer: {
+    proxy: {
+      "/graphql": {
+        target: "http://localhost:5000"
+      }
+    }
+  },
+  configureWebpack: {
+    plugins: [
+      new FlowBabelWebpackPlugin()
+    ],
+    resolve: {
+      alias: {
+        graphql$: path.resolve(__dirname, 'node_modules/graphql/index.js')
+      },
+      extensions: [ '.flow', '.mjs', '.js', '.json', '.jsx', '.css' ]
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/,
+          query: {
+            cacheDirectory: true,
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      ]
+    }
+  }
+}
